0e25237272a570e5c049988d370e0e99865235f2
Victor Realpe <victormanuelrealpepopayan@gmail.com>
2021-11-09 12:33:42 -0500

    primer commit

--- src/pages/ventas.jsx
+++ src/pages/ventas.jsx
@@ -4,17 +4,57 @@ import React from 'react';
 
 function Ventas (){
 
-    return(
-        <div>
-            <header>
-                <h1>
-                    Gestion de Ventas
-                </h1>
-            </header>
-            <form action="">
-                    <input type="text" class="barra-busqueda" id="barra-busqueda" placeholder="Buscar" onclick=""/>
-            </form>
+
+const ventas = [
+    {
+        producto:"netflix",
+        cantidad:1,
+        estado:"activo"
+    },
+    {
+        producto:"netflix",
+        cantidad:4,
+        estado:"cancelado"
+    },
+    {
+        producto:"HBO",
+        cantidad:2,
+        estado:"activo"
+    },
+    {
+        producto:"Dysney +",
+        cantidad:5,
+        estado:"activo"
+    },
+    {
+        producto:"Amazon prime",
+        cantidad:1,
+        estado:"activo"
+    },
+
+]
+
+const Ventas = () => {
+
+    const [mostrarTabla, setMostrarTabla] = useState(true);
+    const [ventas, setVentas] = useState([]);
+
+    useEffect(() =>{
+        //obtener lista de ventas desde el frontend
+        
+    }, []);
+
+    return (
+    <div className="h-full ml-14">
+        <Layout/>
+        <div  className="bg-blue-400 flex  items-center pl-4 border-4 border-light-gray-500 border-opacity-50 h-20 " >
+             <h2 className="font-bold font-serif text-5xl text-white ">Administracion de Ventas</h2>
         </div>
+        <button onClick={()=> {setMostrarTabla(!mostrarTabla)}}  type="button" class="btn btn-success m-6 ">AGREGAR VENTA</button>
+        {mostrarTabla ? <TablaVentas listaVentas={ventas} /> : <FormularioVentas />}
+
+        
+    </div>
 
 
     );
