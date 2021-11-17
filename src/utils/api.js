c238a548d9878e257e4a77c430c83f83b6d6905c
Victor Realpe <victormanuelrealpepopayan@gmail.com>
2021-11-17 15:20:39 -0500

    cambio de url api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
- const baseURL = "http://localhost:5000"
-//const baseURL = 'https://enigmatic-shelf-42481.herokuapp.com';
+//const baseURL = "http://localhost:5000"
+const baseURL = 'https://protected-taiga-86270.herokuapp.com/';
 
 const getToken = () => {
   return `Bearer ${localStorage.getItem('token')}`;
